660294dbebb5215f0747a9e3289435799101cf51
Wagner <wagnetto@protonmail.com>
2024-04-20 18:35:11 -0300

    feature/Add countdown styles

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import { Play } from 'phosphor-react'
+import {
+  HomeContainer,
+  CountDownContainer,
+  FormContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em: </label>
+          <input type="text" id="task" />
+
+          <label htmlFor="minutesAmount">durante: </label>
+          <input
+            type="number"
+            id="minutesAmount
+        "
+          />
+          <span>minutos.</span>
+        </FormContainer>
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}

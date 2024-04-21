3fed52aa6c23f3f52dc9afe6b9a8258e35352e5f
Wagner <wagnetto@protonmail.com>
2024-04-21 08:27:48 -0300

    refactor/Add input validation schema

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import {
   HomeContainer,
   CountDownContainer,
@@ -9,15 +11,29 @@ import {
   Separator,
   StartCountdownButton,
 } from './styles'
-import { useState } from 'react'
+
+const newCycleValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+type NewCycleFormData = zod.infer<typeof newCycleValidationSchema>
 
 export function Home() {
-    const { register, handleSubmit, watch } = useForm()
-    function handleCreateNewCycle(data: any) {
-      console.log(data)
-    }
-    const task = watch('task')
-    const isSubmitDisabled = !task
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleValidationSchema),
+    defaultValues: { task: '', minutesAmount: 0 },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">

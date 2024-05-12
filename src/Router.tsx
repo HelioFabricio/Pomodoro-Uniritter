5847373993944a7ee6e7bdf89911ce631e64098f
Hélio Fabricio <65051794+HelioFabricio@users.noreply.github.com>
2024-05-12 15:57:14 -0300

    Feature/Add history component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { DefaultLayout } from './components/layouts/DefaultLayout'
+import { History } from './pages/History'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
       </Route>
     </Routes>
   )
